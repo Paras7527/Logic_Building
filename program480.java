ffb696b1b4bb32471176c2be5a35477d2cb54025
Paras Kulkarni <paraskulkarni7527@gmail.com>
2025-02-18 10:11:10 +0530

    Add files via upload

---
+++ program480.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.*;
+
+class program479 {
+    public static void main(String Arg[]) throws Exception 
+    {
+        String DirName = "", PackName = "",Header = "";
+        FileOutputStream fopackobj = null;
+        File fobj = null;
+        FileInputStream fiobj = null;
+        File Packobj = null;
+        Scanner sobj = new Scanner(System.in);
+        int i = 0, j = 0, iRet;
+        boolean bret = false;
+        byte Buffer[] = new byte[1024];
+
+        try{
+            System.out.println("---------------------------------------------------------------------");
+            System.out.println("-------------- Marvellous Packer Unpacker CUI Module ----------------");
+            System.out.println("---------------------------------------------------------------------");
+
+
+            System.out.println("---------------------------- Packing Activity -----------------------");
+
+            System.out.println("Enter the name of directory that you want to open for packing : ");
+            DirName = sobj.nextLine();
+
+            System.out.println("Enter the name of packed file that you want to create : ");
+            PackName = sobj.nextLine();
+
+        
+
+            bret = Packobj.createNewFile();
+            if (bret == true) {
+                System.out.println("Packed file gets succesfully created with the name : " + PackName);
+            } else {
+                System.out.println("Unable to proceed as pack file is not created");
+                return;
+            }
+
+            fopackobj = new FileOutputStream(Packobj);
+
+            fobj = new File(DirName);
+
+            if (fobj.exists()) {
+                System.out.println("Directory is succesfully opened");
+
+                File Arr[] = fobj.listFiles();
+
+                System.out.println("Total number of files in the Directory are : " + Arr.length);
+
+                for (i = 0; i < Arr.length; i++) {
+
+                    if ((Arr[i].getName()).endsWith(".txt"))
+                    {
+                        Header = Arr[i].getName() + " " + Arr[i].length();
+
+                        for (j = Header.length(); j < 100; j++) {
+                            Header = Header + " ";
+                        }
+                        // Header Writing
+                        fopackobj.write(Header.getBytes(), 0, 100);
+                        fiobj = new FileInputStream(Arr[i]);
+
+                        // File Data Writing
+                        while ((iRet = fiobj.read(Buffer)) != -1) {
+                            fopackobj.write(Buffer, 0, iRet);
+                        }
+                        fiobj.close();
+                        Header = "";
+                    }
+                }
+            } else {
+                System.out.println("There is no such director");
+            }
+            System.out.println("---------------------------------------------------------------------");
+        }
+        catch(Exception eobj){}
+    }
+}
